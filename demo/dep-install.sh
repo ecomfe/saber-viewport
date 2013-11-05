691711f6a3f5030796ede1ed9d91178393df2ae8
treelite <c.xinle@gmail.com>
2013-11-05 17:38:29 +0800

    update saber-tap version

--- demo/dep-install.sh
+++ demo/dep-install.sh
@@ -5,7 +5,7 @@ edp import saber-lang@0.2.0
 edp import saber-dom@0.3.0
 edp import saber-promise@0.1.0
 edp import saber-run@0.1.0
-edp import saber-tap@0.1.1
+edp import saber-tap@0.1.0
 edp import er@3.0.2
 
 echo 'done.'
